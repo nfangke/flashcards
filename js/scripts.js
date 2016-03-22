8ec7c45ffb016d794c843cdbbc309ddc359e98de
Ashley Maceli and Nancy Fang <nancy@>
2016-03-22 14:43:58 -0700

    Flashcards made with JS

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $(".flash-card1").click(function() {
+    $(".def-showing1").toggle();
+    $(".def-hidden1").toggle();
+  });
+
+  $(".flash-card2").click(function() {
+    $(".def-showing2").toggle();
+    $(".def-hidden2").toggle();
+  });
+
+  $(".flash-card3").click(function() {
+    $(".def-showing3").toggle();
+    $(".def-hidden3").toggle();
+  });
+
+  $(".flash-card4").click(function() {
+    $(".def-showing4").toggle();
+    $(".def-hidden4").toggle();
+  });
+});
